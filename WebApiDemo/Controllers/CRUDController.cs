15a2472d2ef2cc2db4d51122d2610aef454096ff
brain-u <78217760+brain-u@users.noreply.github.com>
2021-02-02 23:37:06 +0800

    加入專案檔案。

---
+++ WebApiDemo/Controllers/CRUDController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApiDemo.DBEntity;
+using System.Net.Http;
+
+namespace WebApiDemo.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        public ActionResult Index()
+        {
+            IEnumerable<Employee> employeeData = null;
+            HttpClient hc = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44310/api/employee")
+            };
+
+            var consumeapi = hc.GetAsync("employee");
+            consumeapi.Wait();
+
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode) {
+                System.Threading.Tasks.Task<IList<Employee>> displaydata = readdata.Content.ReadAsAsync<IList<Employee>>();
+                displaydata.Wait();
+                employeeData = displaydata.Result;
+            }
+            return View(employeeData);
+        }
+    }
+}
---
+++ WebApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using WebApiDemo.DBEntity;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetEmployee() {
+            VertrustDemoEntities em = new VertrustDemoEntities();
+            List<Employee> result = em.Employees.ToList();
+
+            return Ok(result);
+
+        }
+    }
+}
