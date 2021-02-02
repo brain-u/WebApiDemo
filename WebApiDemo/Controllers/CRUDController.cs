b85645b36a34342ab030d559b324fdd1ffceabca
brain-u <78217760+brain-u@users.noreply.github.com>
2021-02-03 03:25:36 +0800

    20210203 刪除

--- WebApiDemo/Controllers/CRUDController.cs
+++ WebApiDemo/Controllers/CRUDController.cs
@@ -56,5 +56,91 @@ namespace WebApiDemo.Controllers
             }
             return View("Create");
         }
+
+        // GET: CRUD
+        public ActionResult Detail(int id)
+        {
+            Employee EmpObj = null;
+            HttpClient hc = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44310/api/employee")
+            };
+
+            var consumeapi = hc.GetAsync("employee?id=" + id.ToString());
+            consumeapi.Wait();
+
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                System.Threading.Tasks.Task<Employee> DisplayData = readdata.Content.ReadAsAsync<Employee>();
+
+                DisplayData.Wait();
+                EmpObj = DisplayData.Result;
+            }
+            return View(EmpObj);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Employee EmpObj = null;
+            HttpClient hc = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44310/api/employee")
+            };
+
+            var consumeapi = hc.GetAsync("employee?id=" + id.ToString());
+            consumeapi.Wait();
+
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                System.Threading.Tasks.Task<Employee> DisplayData = readdata.Content.ReadAsAsync<Employee>();
+
+                DisplayData.Wait();
+                EmpObj = DisplayData.Result;
+            }
+            return View(EmpObj);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee em)
+        {
+            HttpClient hc = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44310/api/employee")
+            };
+            System.Threading.Tasks.Task<HttpResponseMessage> InsertRecord = hc.PutAsJsonAsync<Employee>("Employee", em);
+            InsertRecord.Wait();
+
+            var SaveData = InsertRecord.Result;
+
+            if (SaveData.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message("無可更新員工資料");
+            }
+            return View(em);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44310/api/Employee");
+
+            var delrecord = hc.DeleteAsync("Employee/" + id.ToString());
+            delrecord.Wait();
+
+            var displaydatee = delrecord.Result;
+            if (displaydatee.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
     }
 }
--- WebApiDemo/Controllers/EmployeeController.cs
+++ WebApiDemo/Controllers/EmployeeController.cs
@@ -6,12 +6,13 @@ using System.Web.Mvc;
 using System.Web.Http;
 using WebApiDemo.DBEntity;
 using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using System.Data.Entity;
 
 namespace WebApiDemo.Controllers
 {
     public class EmployeeController : ApiController
     {
-        private readonly VertrustDemoEntities em = new VertrustDemoEntities();
+        private VertrustDemoEntities em = new VertrustDemoEntities();
 
         public IHttpActionResult GetEmployee() {
             
@@ -27,6 +28,58 @@ namespace WebApiDemo.Controllers
             em.Employees.Add(EmpData);
             em.SaveChanges();
 
+            return Ok();
+
+        }
+
+        public IHttpActionResult GetEmployeeId(int id)
+        {
+            Employee EmpDetail = null;
+
+            EmpDetail = em.Employees.Where(x => x.EmployeeID == id).Select(x => new Employee()
+            {
+                EmployeeID = x.EmployeeID,
+                Name = x.Name,
+                Position = x.Position,
+                Age = x.Age,
+                Salary = x.Salary,
+            }).FirstOrDefault();
+
+            if (EmpDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(EmpDetail);
+
+        }
+
+        public IHttpActionResult Put(Employee ec)
+        {
+            Employee UpDataEmp = em.Employees.Where(x => x.EmployeeID == ec.EmployeeID).FirstOrDefault<Employee>();
+
+            if (UpDataEmp != null)
+            {
+                UpDataEmp.EmployeeID = ec.EmployeeID;
+                UpDataEmp.Name = ec.Name;
+                UpDataEmp.Position = ec.Position;
+                UpDataEmp.Age = ec.Age;
+                UpDataEmp.Salary = ec.Salary;
+                em.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            Employee empdel = em.Employees.Where( x => x.EmployeeID == id ).FirstOrDefault();
+            em.Entry(empdel).State = System.Data.Entity.EntityState.Deleted;
+            em.SaveChanges();
             return Ok();
 
         }
